227e97ba72a8f2835e96ebe92c902d856641a203
Udayanga <udayanga.wijesiri@gmail.com>
2024-12-25 17:46:50 +0530

    implement add products to the table

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const showFormBtn = document.getElementById("show-product-add-form");
 const addProductBtn = document.getElementById("add-product-btn");
 const closeFormBtn = document.getElementById("close-product-add-form");
+let productIncrement=1000;
+let productList = [];
 
 showFormBtn.addEventListener("click", () => toggleShowForm("show"));
 closeFormBtn.addEventListener("click", () => toggleShowForm("close"));
-addProductBtn.addEventListener("click", () => console.log("clicked"))
+addProductBtn.addEventListener("click", () => addProduct());
 
 function toggleShowForm(operation) {
   const form = document.getElementById("add-product");
@@ -17,4 +19,44 @@ function toggleShowForm(operation) {
     form.classList.add("d-none");
     showFormBtn.classList.remove("d-none");
   }
+}
+
+function addProduct(){
+  productIncrement++;
+  const productName = document.getElementById("product-name");
+  const productPrice = document.getElementById("product-price");
+  const productDiscount = document.getElementById("product-discount");
+  const productID = "B" + productIncrement;
+  let htmlEl=document.getElementById("table-body");
+  
+  const product = {
+    id: productID,
+    name: productName.value,
+    price: productPrice.value,
+    discount: productDiscount.value
+  }
+
+  productList.push(product);
+
+  addProductToTable(productList, htmlEl);
+  
+  productName.value="";
+  productPrice.value="";
+  productDiscount.value="";
+  
+}
+
+function addProductToTable(array, htmlEl){
+  let tableContent = '';
+  array.forEach(element => {
+    tableContent += `
+    <tr>
+      <td>${element.id}</td>
+      <td>${element.name}</td>
+      <td>${element.price}</td>
+      <td>${element.discount}</td>
+    </tr>
+  `;
+  });
+  htmlEl.innerHTML = tableContent;  
 }
